72c5f4a2401da1b0ad77c99aa1427747bd76fc1b
Sayan Dutta <duttasayan.7@gmail.com>
2018-09-18 17:18:54 +0530

    removed directory and put everything under root

--- syn01/programs/counter.s
+++
@@ -1,6 +0,0 @@
-JMP @_start             ; jump to entry point
-
-_start: ADD R0, 0x1     ; add 1 to R0
-        ST 0xff, R0     ; write R0 contents to PPI data port
-        JMP @_start     ; go back to start
-        HLT             ; halt the cpu should not reach here
--- syn01/programs/test1.s
+++
@@ -1,4 +0,0 @@
-JMP @_start             ; Jump to the entry point
-
-_start: MOV R0, 0xaf    ; should load 0xaf into R0
-        HLT             ; Halt the cpu
